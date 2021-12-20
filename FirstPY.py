13f7b7b64e4a729a77c7cf92dd76e13684f995a2
IamOppy <randolffretrato@yahoo.com>
2021-12-19 18:27:17 -0800

    Second Commit

--- FirstPY.py
+++ FirstPY.py
@@ -3,6 +3,8 @@ from sys import exit
 import os
 from random import randint
 
+from pygame.constants import K_SPACE, KEYDOWN
+
 pygame.init()
 
 WIDTH = 800
@@ -22,47 +24,87 @@ SNAIL_RECT = SNAIL_SURFACE.get_rect(bottomright=(600, 250))
 TEXT_SURFACE = FONT_.render("My Game", False, "Black")
 TEXT_RECT = TEXT_SURFACE.get_rect(center=(400, 50))
 
-ENEMY_SURFACE = pygame.image.load(os.path.join(
+PLAYER_SURFACE = pygame.image.load(os.path.join(
     "BuildingBlocks/Players", "Player_stand.png")).convert_alpha()
-ENEMY_RECT = ENEMY_SURFACE.get_rect(midbottom=(80, 250))
+PLAYER_RECT = PLAYER_SURFACE.get_rect(midbottom=(80, 250))
+PLAYER_GRAVITY = 0
+GAME_RUNNING = True
 
 
 def draw_window():
     SCREEN.blit(SKY_SURFACE, (0, 0))
     SCREEN.blit(GROUND_SURFACE, (0, 250))
-    SCREEN.blit(TEXT_SURFACE, (TEXT_RECT))
+
+    pygame.draw.rect(SCREEN, "purple", TEXT_RECT)
+    pygame.draw.rect(SCREEN, "purple", TEXT_RECT, 10)
+    pygame.draw.line(SCREEN, "Black", start_pos=(0, 0), end_pos=(800, 400))
+    pygame.draw.ellipse(SCREEN, "black", pygame.Rect(50, 150, 100, 100))
+
+    SCREEN.blit(TEXT_SURFACE, TEXT_RECT)
     SCREEN.blit(SNAIL_SURFACE, SNAIL_RECT)
-    SCREEN.blit(ENEMY_SURFACE, ENEMY_RECT)
+    SCREEN.blit(PLAYER_SURFACE, PLAYER_RECT)
 
 
 def SNAIL_POS_RESET():
-    SNAIL_RECT.x -= 1
+    SNAIL_RECT.x -= 5
     if SNAIL_RECT.right <= 0:
         SNAIL_RECT.left = 800
 
 
-def collision_detections_Key_Detections():
+def collision_detections_MouseKey_Detections():
     key_detection = pygame.mouse.get_pressed()
     mouse_pos = pygame.mouse.get_pos()
-    if ENEMY_RECT.colliderect(SNAIL_RECT):
+    if PLAYER_RECT.colliderect(SNAIL_RECT):
         print('Collided')
-    elif ENEMY_RECT.collidepoint(mouse_pos):
+    elif PLAYER_RECT.collidepoint(mouse_pos):
         print("Mouse Detected on Object")
         print(key_detection)
+
+
+def actions():
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_SPACE]:
+        print('Key pressed')
+
+
+def border_limit():
+    if PLAYER_RECT.bottom > 250:
+        PLAYER_RECT.bottom = 250
+
+
+def game_state_end():
+    global GAME_RUNNING
+    if SNAIL_RECT.colliderect(PLAYER_RECT):
+        GAME_RUNNING = False
+        SCREEN.fill("white")
 
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.exit()
             exit()
-        if event.type == pygame.MOUSEMOTION:
-            if ENEMY_RECT.collidepoint(event.pos):
-                print("Mouse Object detected on Enemy")
 
-    SNAIL_POS_RESET()
-    # collision_detections_Key_Detections()
-    draw_window()
+        if GAME_RUNNING:
+            if event.type == pygame.MOUSEMOTION:
+                if PLAYER_RECT.collidepoint(event.pos):
+                    PLAYER_GRAVITY = -20
+            if event.type == pygame.KEYDOWN:
+                if event.key == K_SPACE and PLAYER_RECT.bottom == 250:  # IF PLAYER HITS THE GROUND, THE PLAYER CAN JUMP
+                    PLAYER_GRAVITY = -20
+        else:
+            if event.type == pygame.KEYDOWN and event.key == K_SPACE:
+                GAME_RUNNING = True
+                SNAIL_RECT.left = 800
 
-    pygame.display.update()
-    CLOCK.tick(60)
+    if GAME_RUNNING:
+        PLAYER_GRAVITY += 1
+        PLAYER_RECT.y += PLAYER_GRAVITY
+
+        SNAIL_POS_RESET()
+        border_limit()
+        # collision_detections_MouseKey_Detections()
+        draw_window()
+        game_state_end()
+
+        pygame.display.update()
+        CLOCK.tick(60)
